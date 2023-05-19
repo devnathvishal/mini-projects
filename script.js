4585af5c9390ff802f93b674775c3fa24a550ccb
vishal <vishal@vd.com>
2023-05-19 13:48:14 +0530

    Dice project

---
+++ script.js
@@ -0,0 +1,30 @@
+let images = ["one.svg", "two.svg","three.svg",
+"four.svg","five.svg","six.svg",];
+
+let dice = document.querySelectorAll("img");
+
+function roll()
+{
+    dice.forEach(function(die)
+    {
+die.classList.add("shake");
+    });
+
+    setTimeout(function ()
+     
+    {
+        dice.forEach(function(die)
+        {
+            die.classList.remove("shake");
+        });
+        let dieOneValue = Math.floor(Math.random()*6);
+        let dieTwoValue = Math.floor(Math.random()*6);
+
+        document.querySelector("#die-1").setAttribute("src",images[dieOneValue]);
+        document.querySelector("#die-2").setAttribute("src",images[dieTwoValue]);
+        document.querySelector("#total").innerHTML = "Your roll is " + ((dieOneValue+1)+(dieTwoValue+1));
+      },1000
+      );
+}
+
+roll ();
